a780439ec37b1a89288e736e7877f0b91af015da
Nonô Correa <correa.advg@gmail.com>
2024-02-12 13:20:27 -0300

    entrega de desafio listener de clicks

--- index.js
+++ index.js
@@ -4,16 +4,15 @@ const $buttonPaperPlayer1 = document.querySelector(".button-move-paper-1")
 const $buttonScissorsPlayer1 = document.querySelector(".button-move-scissors-1")
 const $moveBox1=document.querySelector('#move-box-1')
 
-function handleStone1Move () {
-   $moveBox1.innerHTML='<img src="./c" >'
-   /*colocar o : alt="imagem pedra" title="imagem pedra"*/
+function handleStone1Move () { 
+   $moveBox1.innerHTML='<img src="image/stone.png" alt="imagem mão branca desenhada com traços em preto" title="imagem mão branca representando pedra do joquempo">'
 }
 
 function handlePaper1Move () {
-    $moveBox1.innerHTML='<img src="/imagens/paper.png">'
+    $moveBox1.innerHTML='<img src="image/paper.png" alt="imagem mão branca desenhada com traços em preto" title="imagem mão branca representando papel do joquempo">'
  }
  function handleScissors1Move() {
-   $moveBox1.innerHTML='<img src="/imagens/scissors.png">'
+   $moveBox1.innerHTML='<img src="image/scissors.png" alt="imagem mão branca desenhada com traços em preto" title="imagem mão branca representando tesoura do joquempo">'
  }
 
 $buttonStonePlayer1.addEventListener("click", handleStone1Move  );
@@ -28,15 +27,14 @@ const $buttonScissorsPlayer2 = document.querySelector(".button-move-scissors-2")
 const $moveBox2=document.querySelector('#move-box-2')
 
 function handleStone2Move () {
-   $moveBox2.innerHTML='<img src="/imagens/stone.png" >'
-   /*colocar o : alt="imagem pedra" title="imagem pedra"*/
+   $moveBox2.innerHTML='<img src="image/stone.png" alt="imagem mão branca desenhada com traços em preto" title="imagem mão branca representando pedra do joquempo">'
 }
 
 function handlePaper2Move () {
-    $moveBox2.innerHTML='<img src="/imagens/paper.png">'
+    $moveBox2.innerHTML='<img src="image/paper.png" alt="imagem mão branca desenhada com traços em preto" title="imagem mão branca representando papel do joquempo">'
  }
  function handleScissors2Move() {
-   $moveBox2.innerHTML='<img src="/imagens/scissors.png">'
+   $moveBox2.innerHTML='<img src="image/scissors.png" alt="imagem mão branca desenhada com traços em preto" title="imagem mão branca representando tesoura do joquempo">'
  }
 
 $buttonStonePlayer2.addEventListener("click", handleStone2Move  );
